92b19e4d56e1a27d1b6623949542b8e64a0526bc
Niklas Kiermeyer <niklaskiermeyer@gmx.de>
2024-03-18 16:23:15 +0100

    created train notebooks for train,val, test training

--- src/druxai/train.py
+++ src/druxai/train.py
@@ -4,11 +4,10 @@ import os
 
 import torch
 import torch.nn as nn
-import wandb
 from torch.nn.utils import clip_grad_norm_
 from torch.optim import SGD
 from torch.optim.lr_scheduler import ExponentialLR
-from torch.utils.data import DataLoader, Subset
+from torch.utils.data import DataLoader
 from tqdm import tqdm
 
 import pandas as pd
@@ -20,19 +19,20 @@ from druxai.utils.dataframe_utils import create_batch_result
 # -----------------------------------------------------------------------------
 # training params
 batch_size = 512
-learning_rate = 0.01
+learning_rate = 0.001
 fold = 1
-epochs = 2
-train_on_subset = True
+epochs = 3
+train_on_subset = False
+subset_size = 5000
 seed = 1337
 grad_clip = 1.0
-on_subset = False
 device = torch.device("mps")  # examples: 'cpu', 'cuda', 'cuda:0', 'cuda:1', 'mps' etc.
 PATH = "/Users/niklaskiermeyer/Desktop/Codespace/DruxAI/results/training"
 # wandb logging
-wandb_log = False
+wandb_log = True
 wandb_project_name = "druxai"
-wandb_run_name = "druxai-run1"
+wandb_run_name = "druxai-run0"
+# added to not get AttributeError: module '__main__' has no attribute '__spec__'
 __spec__ = None
 # -----------------------------------------------------------------------------
 config_keys = [k for k, v in globals().items() if not k.startswith("_") and isinstance(v, (int, float, bool, str))]
@@ -40,6 +40,16 @@ with open("/Users/niklaskiermeyer/Desktop/Codespace/DruxAI/src/druxai/utils/conf
     exec(f.read())  # overrides from command line or config file
 config = {k: globals()[k] for k in config_keys}  # will be useful for logging
 # -----------------------------------------------------------------------------
+
+# Initialize wandb run if wandb_log is True
+if wandb_log:
+    import wandb
+
+    wandb.init(
+        project=wandb_project_name,
+        name=wandb_run_name,
+        config=config,
+    )
 
 
 @torch.no_grad()
@@ -85,14 +95,6 @@ def train():
     if not os.path.exists(PATH):
         os.makedirs(PATH)
 
-    # Initialize wandb run
-    if wandb_log:
-        wandb.init(
-            project=wandb_project_name,
-            name=wandb_run_name,
-            config=config,
-        )
-
     # Hard coded for now, should be improved in the future
     ds = MyDataSet(file_path="../../data/preprocessed", results_dir="../../results", n_splits=5)
     model = Interaction_Model(ds)
@@ -105,22 +107,26 @@ def train():
     scheduler1 = ExponentialLR(optimizer1, gamma=0.9)  # gamma 0.9 #0.95 only for 100 epochs
     scheduler2 = ExponentialLR(optimizer2, gamma=0.9)  # gamma 0.9
 
-    ds.change_fold(fold, "train")
+    # Small subset for testing
+    # if train_on_subset:
+    #     subset_indices = range(0, subset_size)
+    #     subset_dataset = Subset(ds, subset_indices)
+    #     dl = DataLoader(subset_dataset, batch_size=batch_size, shuffle=True, num_workers=0, pin_memory=True)
+    # else:
+    #     dl = DataLoader(ds, batch_size=batch_size, shuffle=True, num_workers=0, pin_memory=True)
+
+    dataset_ids = ds.generate_train_val_test_ids()
+    ds.preprocess_train_val_test(dataset_ids)
+
+    # Create DataLoader instances for train and validation datasets
+
+    ds.selected_dataset = "train"
+    train_loader = DataLoader(ds, batch_size=batch_size, shuffle=True)
+
     for epoch in range(1, epochs + 1):  # been without +1
-
-        # Small subset for testing
-        if on_subset:
-            subset_indices = range(0, 5000)
-            subset_dataset = Subset(ds, subset_indices)
-            dl = DataLoader(subset_dataset, batch_size=batch_size, shuffle=True, num_workers=8, pin_memory=True)
-
-        else:
-            dl = DataLoader(ds, batch_size=batch_size, shuffle=True, num_workers=8, pin_memory=True)
-
         model.train()
         train_loss = 0.0
-        for drug, molecular, outcome, _ in tqdm(dl):
-            assert ds.mode == "train", "training on " + ds.mode + " dataset is not possible"
+        for drug, molecular, outcome, _ in tqdm(train_loader):
 
             drug, molecular, outcome = drug.to(device), molecular.to(device), outcome.to(device)
 
@@ -144,16 +150,13 @@ def train():
             selected_optimizer = optimizer1 if torch.rand(1) < 0.5 else optimizer2
             selected_optimizer.step()
 
-        ds.change_fold(fold, "test")
-        assert ds.mode == "test", "testing on " + ds.mode + " dataset is not possible"
-
-        dl_test = DataLoader(ds, batch_size=1000, shuffle=False, num_workers=8, pin_memory=True)
-        avg_test_loss = evaluate_model(model, ds, dl_test, device, fold, epoch, PATH, epochs)
+        ds.selected_dataset = "val"
+        val_loader = DataLoader(ds, batch_size=batch_size, shuffle=True)
+        avg_test_loss = evaluate_model(model, ds, val_loader, fold, epoch, PATH, epochs)
+        ds.selected_dataset = "train"
 
         if wandb_log:
             wandb.log({"epoch": epoch, "epoch loss": train_loss, "test_loss": avg_test_loss})
-
-        ds.change_fold(fold, "train")
 
         scheduler1.step()
         scheduler2.step()
