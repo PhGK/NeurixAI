64b19e450ded5f5b3042672c3da48ff5b1e14181
Niklas Kiermeyer <niklaskiermeyer@gmx.de>
2024-03-20 14:08:17 +0100

    update

--- src/druxai/train.py
+++ src/druxai/train.py
@@ -1,6 +1,7 @@
 """Training script. To run it do: $ python train.py --batch_size=32."""
 
 import os
+from typing import Tuple
 
 import torch
 import torch.nn as nn
@@ -13,12 +14,17 @@ from tqdm import tqdm
 
 import pandas as pd
 
-from druxai._logging import logger
+from druxai._logging import _setup_logger
 from druxai.models.NN import Interaction_Model
 from druxai.utils.data import MyDataSet
+from druxai.utils.dataframe_utils import create_batch_result
 
 # -----------------------------------------------------------------------------
-# training params
+# path which need to be defined
+RESULTS_PATH = "/Users/niklaskiermeyer/Desktop/Codespace/DruxAI/results/training"
+PROJECT_PATH = "/Users/niklaskiermeyer/Desktop/Codespace/DruxAI"
+DATA_PATH = "/Users/niklaskiermeyer/Desktop/Codespace/DruxAI/data/preprocessed"
+# NN training params
 batch_size = 512
 learning_rate = 0.001
 fold = 1
@@ -28,8 +34,11 @@ subset_size = 5000
 seed = 1337
 grad_clip = 1.0
 device = torch.device("mps")  # examples: 'cpu', 'cuda', 'cuda:0', 'cuda:1', 'mps' etc.
-PATH = "/Users/niklaskiermeyer/Desktop/Codespace/DruxAI/results/training"
+criterion = nn.HuberLoss()
+# Use Preloaded weights "resume" from os.path.join(RESULTS_PATH, "ckpt.pt") or start from scratch
 init_from = "scratch"
+# Logging config
+logger = _setup_logger(log_file_path=os.path.join(RESULTS_PATH, "logfile.log"))
 # wandb logging
 wandb_log = True
 wandb_project_name = "druxai"
@@ -38,7 +47,7 @@ wandb_run_name = "druxai-run1"
 __spec__ = None
 # -----------------------------------------------------------------------------
 config_keys = [k for k, v in globals().items() if not k.startswith("_") and isinstance(v, (int, float, bool, str))]
-with open("/Users/niklaskiermeyer/Desktop/Codespace/DruxAI/src/druxai/utils/configurator.py") as f:
+with open(os.path.join(PROJECT_PATH, "src", "druxai", "utils", "configurator.py")) as f:
     exec(f.read())  # overrides from command line or config file
 config = {k: globals()[k] for k in config_keys}  # will be useful for logging
 # -----------------------------------------------------------------------------
@@ -54,21 +63,27 @@ if wandb_log:
     )
 
 
-def evaluate_model(model, ds, dataloader_val, fold, epoch, PATH, epochs):
-    """Evaluate Model after each epoch of training and saves final prediction for last epoch.
+def evaluate_model(
+    model: torch.nn.Module, dataloader_val: DataLoader, val_data: MyDataSet, epoch: int, save_evaluation: bool = True
+) -> Tuple[float, float]:
+    """Evaluate the performance of a neural network model on a validation dataset.
+
+    This function evaluates the given `model` on the provided `val_data` using the `dataloader_val`.
+    It calculates the average loss and the Pearson correlation coefficient (r score) for the predictions.
 
     Args:
-        model (_type_): _description_
-        ds (_type_): _description_
-        dataloader_val (_type_): Dataloader which is being evaluated.
-        fold (_type_): _description_
-        epoch (_type_): _description_
-        PATH (_type_): _description_
+        model (torch.nn.Module): The neural network model being evaluated.
+        dataloader_val (DataLoader): DataLoader containing validation data.
+        val_data (MyDataSet): Validation dataset.
+        epoch (int): The current epoch number.
+        save_evaluation (bool, optional): Whether to save the final predictions. Defaults to True.
 
     Returns
     -------
-        total_loss: average test loss for batch
-        r_score: Pearson correlation coefficient (r score) for the predictions
+        Tuple[float, float]: A tuple containing the average loss and the Pearson correlation coefficient (r score).
+
+    Example:
+        avg_loss, r_score = evaluate_model(model, dataloader_val, val_data, 5, save_evaluation=True)
     """
     model.eval()
     prediction_frames = []
@@ -76,21 +91,22 @@ def evaluate_model(model, ds, dataloader_val, fold, epoch, PATH, epochs):
     all_predictions = []
     val_total_loss = 0.0
 
-    for drug, molecular, outcome, _ in tqdm(dataloader_val):
+    for drug, molecular, outcome, idx in tqdm(dataloader_val):
         drug, molecular, outcome = drug.to(device), molecular.to(device), outcome.to(device)
 
         with torch.no_grad():
             prediction = model(drug, molecular)
-            loss = nn.HuberLoss()(outcome, prediction)
+            loss = criterion(outcome, prediction)
             val_total_loss += loss.item()
-
-        # TODO: bad dependency, make criterion instead of hardcoding huber loss
-        loss = nn.HuberLoss()(outcome, prediction)
 
         all_outcomes.extend(outcome.cpu().detach().numpy().reshape(-1))
         all_predictions.extend(prediction.cpu().detach().numpy().reshape(-1))
-        if epoch == epochs:
-            pd.concat(prediction_frames, axis=0).to_csv(os.path.join(PATH, "prediction.csv"))
+        if epoch == (epochs - 1):
+            batch_result = create_batch_result(outcome, prediction, val_data, idx, epoch)
+            prediction_frames.append(batch_result)
+
+    if save_evaluation & (epoch == (epochs - 1)):
+        pd.concat(prediction_frames, axis=0).to_csv(os.path.join(RESULTS_PATH, "prediction.csv"))
 
     r_score, _ = pearsonr(all_outcomes, all_predictions)
     avg_loss = val_total_loss / len(dataloader_val)
@@ -102,19 +118,19 @@ def train():
     torch.manual_seed(seed)
     torch.cuda.manual_seed(seed)
 
-    if not os.path.exists(PATH):
-        os.makedirs(PATH)
+    if not os.path.exists(RESULTS_PATH):
+        os.makedirs(RESULTS_PATH)
 
     # Some Initalizations
     best_val_loss = 1e5
 
     # Hard coded 2 datasets for now, should be improved in the future; Badly coded; for sure needs to be refactored
     # Works because splits are the same because of same seed
-    train_data = MyDataSet(file_path="../../data/preprocessed", results_dir="../../results", n_splits=5)
+    train_data = MyDataSet(file_path=DATA_PATH, results_dir=RESULTS_PATH, n_splits=5)
     dataset_ids = train_data.generate_train_val_test_ids(seed=seed)
     train_data.preprocess_train_val_test(dataset_ids)
 
-    val_data = MyDataSet(file_path="../../data/preprocessed", results_dir="../../results", n_splits=5)
+    val_data = MyDataSet(file_path=DATA_PATH, results_dir=RESULTS_PATH, n_splits=5)
     dataset_ids = val_data.generate_train_val_test_ids(seed=seed)
     val_data.preprocess_train_val_test(dataset_ids)
 
@@ -128,9 +144,9 @@ def train():
         optimizer2 = SGD(model.nn2.parameters(), momentum=0.9, lr=learning_rate, weight_decay=1e-5)
 
     elif init_from == "resume":
-        logger.info(f"Resuming training from {PATH}/ckpt.pt")
+        logger.info(f"Resuming training from {RESULTS_PATH}/ckpt.pt")
         # Load checkpoint
-        ckpt_path = os.path.join(PATH, "ckpt.pt")
+        ckpt_path = os.path.join(RESULTS_PATH, "ckpt.pt")
         checkpoint = torch.load(ckpt_path, map_location=device)
         # Initialize model with the same configuration as the checkpoint
         model = Interaction_Model(train_data)
@@ -147,8 +163,6 @@ def train():
         model.to(device)
     else:
         raise ValueError("init_from must be either 'scratch' or 'resume'")
-
-    criterion = nn.HuberLoss()
 
     scheduler1 = ExponentialLR(optimizer1, gamma=0.9)  # gamma 0.9 #0.95 only for 100 epochs
     scheduler2 = ExponentialLR(optimizer2, gamma=0.9)  # gamma 0.9
@@ -174,7 +188,6 @@ def train():
     for epoch in range(epochs):
         model.train()
         total_loss = 0.0
-        r2_batch = 0.0
 
         for drug, molecular, outcome, _ in tqdm(train_loader):
             drug, molecular, outcome = drug.to(device), molecular.to(device), outcome.to(device)
@@ -193,19 +206,12 @@ def train():
             selected_optimizer = optimizer1 if torch.rand(1) < 0.5 else optimizer2
             selected_optimizer.step()
 
-            # Calculate Pearson correlation coefficient for the current batch; Computationally not very efficient
-            r2_batch, _ = pearsonr(
-                outcome.cpu().detach().numpy().reshape(-1), prediction.cpu().detach().numpy().reshape(-1)
-            )
-            r2_batch += r2_batch
-
-        avg_r2 = r2_batch / len(train_loader)
         avg_loss = total_loss / len(train_loader)
 
-        avg_val_loss, r2_val = evaluate_model(model, train_data, val_loader, fold, epoch, PATH, epochs)
+        avg_val_loss, r2_val = evaluate_model(model, val_loader, val_data, epoch)
 
         if wandb_log:
-            wandb.log({"train loss": avg_loss, "r2_train": avg_r2, "val_loss": avg_val_loss, "r2_val": r2_val})
+            wandb.log({"train loss": avg_loss, "val_loss": avg_val_loss, "r2_val": r2_val})
 
         if avg_val_loss < best_val_loss:
             best_val_loss = avg_val_loss
@@ -217,8 +223,8 @@ def train():
                 "best_val_loss": best_val_loss,
                 "config": config,
             }
-            logger.info(f"New best val_loss achieved. Saving checkpoint to {PATH}/ckpt.pt")
-            torch.save(checkpoint, os.path.join(PATH, "ckpt.pt"))
+            logger.info(f"New best val_loss achieved. Saving checkpoint to {os.path.join(RESULTS_PATH, 'ckpt.pt')}")
+            torch.save(checkpoint, os.path.join(RESULTS_PATH, "ckpt.pt"))
 
         scheduler1.step()
         scheduler2.step()
--- src/druxai/utils/dataframe_utils.py
+++ src/druxai/utils/dataframe_utils.py
@@ -9,7 +9,7 @@ import pandas as pd
 from pandas import DataFrame
 
 
-def create_batch_result(outcome: Tensor, prediction: Tensor, ds: Any, idx: Any, fold: int, epoch: int) -> DataFrame:
+def create_batch_result(outcome: Tensor, prediction: Tensor, ds: Any, idx: Any, epoch: int) -> DataFrame:
     """
     Create a DataFrame containing batch results.
 
@@ -31,7 +31,6 @@ def create_batch_result(outcome: Tensor, prediction: Tensor, ds: Any, idx: Any, 
             "prediction": prediction.squeeze().cpu().numpy(),
             "cells": np.array(ds.current_cases.loc[np.array(idx), "cell_line"]),
             "drugs": np.array(ds.current_cases.loc[np.array(idx), "DRUG"]),
-            "fold": fold,
             "epoch": epoch,
         }
     )
