722cd0742ace1790e7dad269a5c545d95baf25c0
Niklas Kiermeyer <niklaskiermeyer@gmx.de>
2024-03-22 23:14:24 +0100

    restructured training to work with config.yml file

--- src/druxai/train.py
+++ src/druxai/train.py
@@ -1,21 +1,24 @@
 """Training script. To run it do: $ python train.py --batch_size=32."""
 
-import math
+import argparse
+import logging
 import os
-import random
-from typing import Tuple
+from typing import Callable, Dict, Tuple
 
 import torch
 import torch.nn as nn
+import wandb
+import yaml
 from scipy.stats import pearsonr, spearmanr
 from torch.nn.utils import clip_grad_norm_
-from torch.optim import AdamW
+from torch.optim import AdamW, Optimizer
+from torch.optim.lr_scheduler import ExponentialLR, _LRScheduler
 from torch.utils.data import DataLoader
 from tqdm import tqdm
 
 import pandas as pd
 
-from druxai._logging import _setup_logger
+from druxai._logging import _setup_logger, logger
 from druxai.models.NN_minimal import Interaction_Model
 from druxai.utils.data import DrugResponseDataset
 from druxai.utils.dataframe_utils import (
@@ -24,60 +27,8 @@ from druxai.utils.dataframe_utils import (
     standardize_molecular_data_inplace,
 )
 
-# -----------------------------------------------------------------------------
-# Path which need to be defined SET THESE PARAMETERS
-RESULTS_PATH = "/Users/niklaskiermeyer/Desktop/Codespace/DruxAI/results/training"
-PROJECT_PATH = "/Users/niklaskiermeyer/Desktop/Codespace/DruxAI"
-DATA_PATH = "/Users/niklaskiermeyer/Desktop/Codespace/DruxAI/data/preprocessed"
-batch_size = 128
-epochs = 30
-train_on_subset = False
-subset_size = 100000
-seed = 1337
-grad_clip = 1.0
-device = torch.device("mps")  # examples: 'cpu', 'cuda', 'cuda:0', 'cuda:1', 'mps' etc.
-criterion = nn.HuberLoss()
-num_workers = 6
-init_from = "scratch"  # Use "resume" from os.path.join(RESULTS_PATH, "ckpt.pt") for pretrained weights
-logger = _setup_logger(log_file_path=os.path.join(RESULTS_PATH, "logfile.log"))  # Logging config
-# eval settings
-eval_interval = 100  # after how many batches we evaluate, and how many train_loss uses to approximate
-# learning rate decay settings
-learning_rate = 0.05
-decay_lr = True  # whether to decay the learning rate
-warmup_iters = 200  # how many steps to warm up for
-lr_decay_iters = 1000  # should be ~= max_iters per Chinchilla
-min_lr = 6e-5  # minimum learning rate, should be ~= learning_rate/10 per Chinchilla
-# optimizer params
-optimizer = "AdamW"  # this is only for wandb logging; to set optimizer you need to change code
-weight_decay = 1e-1
-beta1 = 0.9
-beta2 = 0.95
-grad_clip = 1.0  # clip gradients at this value, or disable if == 0.0
-# wandb logging
-wandb_log = True
-wandb_project_name = "druxai"
-wandb_run_name = "druxai-run-1"
-# -----------------------------------------------------------------------------
-# Fixed params
 # added to not get AttributeError: module '__main__' has no attribute '__spec__'
 __spec__ = None
-# -----------------------------------------------------------------------------
-
-
-config_keys = [k for k, v in globals().items() if not k.startswith("_") and isinstance(v, (int, float, bool, str))]
-with open(os.path.join(PROJECT_PATH, "src", "druxai", "utils", "configurator.py")) as f:
-    exec(f.read())  # overrides from command line or config file
-config = {k: globals()[k] for k in config_keys}  # will be useful for logging
-
-if wandb_log:
-    import wandb
-
-    wandb.init(
-        project=wandb_project_name,
-        name=wandb_run_name,
-        config=config,
-    )
 
 
 @torch.no_grad()
@@ -86,6 +37,8 @@ def evaluate_model(
     dataloader_val: DataLoader,
     data: DrugResponseDataset,
     epoch: int,
+    cfg: Dict,
+    loss_func: Callable[[torch.Tensor, torch.Tensor], torch.Tensor],
     save_evaluation: bool = True,
     eval_metric: str = "spearmanr",
 ) -> Tuple[float, float]:
@@ -117,23 +70,23 @@ def evaluate_model(
     losses = torch.zeros(len(dataloader_val))
     for iter_count, (X, y, idx) in tqdm(enumerate(dataloader_val)):
         drug, molecular, outcome = (
-            X["drug_encoding"].to(device),
-            X["gene_expression"].to(device),
-            y.to(device),
+            X["drug_encoding"].to(cfg["DEVICE"]),
+            X["gene_expression"].to(cfg["DEVICE"]),
+            y.to(cfg["DEVICE"]),
         )
 
         prediction = model(drug, molecular)
-        loss = criterion(outcome, prediction)
+        loss = loss_func(outcome, prediction)
         losses[iter_count] = loss.item()
 
         all_outcomes.extend(outcome.cpu().detach().numpy().reshape(-1))
         all_predictions.extend(prediction.cpu().detach().numpy().reshape(-1))
-        if epoch == (epochs - 1):
+        if epoch == (cfg["EPOCHS"] - 1):
             batch_result = create_batch_result(outcome, prediction, data, idx, epoch)
             prediction_frames.append(batch_result)
 
-    if save_evaluation & (epoch == (epochs - 1)):
-        pd.concat(prediction_frames, axis=0).to_csv(os.path.join(RESULTS_PATH, "prediction.csv"))
+    if save_evaluation & (epoch == (cfg["EPOCHS"] - 1)):
+        pd.concat(prediction_frames, axis=0).to_csv(os.path.join(cfg["WEIGHT_DECAY"], "prediction.csv"))
 
     if eval_metric == "spearmanr":
         r_score, _ = spearmanr(all_outcomes, all_predictions)
@@ -147,154 +100,178 @@ def evaluate_model(
     return avg_loss, r_score
 
 
-def get_lr(it: int, warmup_iters: int, lr_decay_iters: int, learning_rate: float, min_lr: float) -> float:
-    """Learning rate decay scheduler with exponential decay.
+def run(cfg: dict, logger: logging.Logger) -> None:
+    """Kick off training."""
+    if cfg["WANDB_LOG"]:
+        wandb.init(
+            project=cfg["WANDB_PROJECT_NAME"],
+            name=cfg["WANDB_RUN_NAME"],
+            config=cfg,
+        )
 
-    Args:
-        it (int): Current iteration.
-        warmup_iters (int): Number of warmup iterations.
-        lr_decay_iters (int): Number of learning rate decay iterations.
-        learning_rate (float): Initial learning rate.
-        min_lr (float): Minimum learning rate.
+    # Loss function
+    loss_func = nn.HuberLoss()
+    torch.manual_seed(cfg["SEED"])
+    torch.cuda.manual_seed(cfg["SEED"])
 
-    Returns
-    -------
-        float: Modified learning rate.
-    """
-    # 1) linear warmup for warmup_iters steps
-    if it < warmup_iters:
-        return learning_rate * it / warmup_iters
-
-    # 2) if it > lr_decay_iters, return min learning rate
-    if it > lr_decay_iters:
-        return min_lr
-
-    # 3) in between, use exponential decay down to min learning rate
-    decay_ratio = (it - warmup_iters) / (lr_decay_iters - warmup_iters)
-    assert 0 <= decay_ratio <= 1
-    coeff = math.exp(-decay_ratio)  # Exponential decay
-    return min_lr + coeff * (learning_rate - min_lr)
-
-
-def train():
-    """End-to-End Training of Druxai Network."""
-    torch.manual_seed(seed)
-    torch.cuda.manual_seed(seed)
-
-    if not os.path.exists(RESULTS_PATH):
-        os.makedirs(RESULTS_PATH)
-
-    # Some Initalizations
-    best_val_loss = 1e9
+    if not os.path.exists(cfg["RESULTS_PATH"]):
+        os.makedirs(cfg["RESULTS_PATH"])
 
     # Load data
-    data = DrugResponseDataset(DATA_PATH)
-    train_id, val_id, test_id = split_data_by_cell_line_ids(data.targets, seed=seed)
+    data = DrugResponseDataset(cfg["DATA_PATH"])
+    train_id, val_id, test_id = split_data_by_cell_line_ids(data.targets, seed=cfg["SEED"])
     standardize_molecular_data_inplace(data, train_id, val_id, test_id)
-    if train_on_subset:
-        train_loader = DataLoader(
-            data,
-            sampler=random.sample(train_id, subset_size),
-            batch_size=batch_size,
-            shuffle=False,
-            pin_memory=True,
-            num_workers=num_workers,
-            persistent_workers=True,
-        )
-        val_loader = DataLoader(
-            data,
-            sampler=random.sample(val_id, int(0.2 * subset_size)),
-            batch_size=batch_size,
-            shuffle=False,
-            pin_memory=True,
-            num_workers=num_workers,
-            persistent_workers=True,
-        )
-    else:
-        train_loader = DataLoader(
-            data,
-            sampler=train_id,
-            batch_size=batch_size,
-            shuffle=False,
-            pin_memory=True,
-            num_workers=num_workers,
-            persistent_workers=True,
-        )
-        val_loader = DataLoader(
-            data,
-            sampler=val_id,
-            batch_size=batch_size,
-            shuffle=False,
-            pin_memory=True,
-            num_workers=num_workers,
-            persistent_workers=True,
-        )
+
+    train_loader = DataLoader(
+        data,
+        sampler=train_id,
+        batch_size=cfg["BATCH_SIZE"],
+        shuffle=False,
+        pin_memory=True,
+        num_workers=cfg["NUM_WORKERS"],
+        persistent_workers=True,
+    )
+    val_loader = DataLoader(
+        data,
+        sampler=val_id,
+        batch_size=cfg["BATCH_SIZE"],
+        shuffle=False,
+        pin_memory=True,
+        num_workers=cfg["NUM_WORKERS"],
+        persistent_workers=True,
+    )
 
     # Determine whether to initialize a new model from scratch or resume training from a checkpoint
-    if init_from == "scratch":
+    best_val_loss = 1e9
+    iter_num = 0
+    if cfg["INIT_FROM"] == "scratch":
         logger.info("Training model from scratch.")
         model = Interaction_Model(data)
-        model.train().to(device)
+        model.train().to(cfg["DEVICE"])
         # Setup optimizers
-        optimizer1 = AdamW(model.nn1.parameters(), learning_rate, betas=(beta1, beta2), weight_decay=weight_decay)
-        optimizer2 = AdamW(model.nn2.parameters(), learning_rate, betas=(beta1, beta2), weight_decay=weight_decay)
-
-    elif init_from == "resume":
-        logger.info(f"Resuming training from {RESULTS_PATH}/ckpt.pt")
+        optimizer1 = AdamW(
+            model.nn1.parameters(),
+            cfg["LEARNING_RATE"],
+            betas=(cfg["BETA1"], cfg["BETA2"]),
+            weight_decay=cfg["WEIGHT_DECAY"],
+        )
+        optimizer2 = AdamW(
+            model.nn2.parameters(),
+            cfg["LEARNING_RATE"],
+            betas=(cfg["BETA1"], cfg["BETA2"]),
+            weight_decay=cfg["WEIGHT_DECAY"],
+        )
+    elif cfg["INIT_FROM"] == "resume":
+        logger.info(f"Resuming training from {cfg['WEIGHT_DECAY']}/ckpt.pt")
         # Load checkpoint
-        ckpt_path = os.path.join(RESULTS_PATH, "ckpt.pt")
+        ckpt_path = os.path.join(cfg["WEIGHT_DECAY"], "ckpt.pt")
         checkpoint = torch.load(ckpt_path)
         # Initialize model with the same configuration as the checkpoint
         model = Interaction_Model(data)
-        model.train().to(device)
+        model.train().to(cfg["DEVICE"])
         model.load_state_dict(checkpoint["model"])
         # Setup optimizers
-        optimizer1 = AdamW(model.nn1.parameters(), learning_rate, betas=(beta1, beta2), weight_decay=weight_decay)
-        optimizer2 = AdamW(model.nn2.parameters(), learning_rate, betas=(beta1, beta2), weight_decay=weight_decay)
+        optimizer1 = AdamW(
+            model.nn1.parameters(),
+            cfg["LEARNING_RATE"],
+            betas=(cfg["BETA1"], cfg["BETA2"]),
+            weight_decay=cfg["WEIGHT_DECAY"],
+        )
+        optimizer2 = AdamW(
+            model.nn2.parameters(),
+            cfg["LEARNING_RATE"],
+            betas=(cfg["BETA1"], cfg["BETA2"]),
+            weight_decay=cfg["WEIGHT_DECAY"],
+        )
         # Also load optimizer states if needed
         optimizer1.load_state_dict(checkpoint["optimizer 1"])
         optimizer2.load_state_dict(checkpoint["optimizer 2"])
         # Update best validation loss
         best_val_loss = checkpoint["best_val_loss"]
         iter_num = checkpoint["iter_num"]
-        model.train().to(device)
+        model.train().to(cfg["DEVICE"])
     else:
         raise ValueError("init_from must be either 'scratch' or 'resume'")
 
-    # training loop
-    iter_num = 0
+    # Set Learning Rate scheduler
+    scheduler1 = ExponentialLR(optimizer1, gamma=0.9)
+    scheduler2 = ExponentialLR(optimizer1, gamma=0.9)
+    train(
+        model=model,
+        loss_func=loss_func,
+        optimizers=(optimizer1, optimizer2),
+        schedulers=(scheduler1, scheduler2),
+        train_loader=train_loader,
+        val_loader=val_loader,
+        data=data,
+        cfg=cfg,
+        iter_num=iter_num,
+        best_val_loss=best_val_loss,
+    )
 
-    # Initialize a circular buffer to store losses of the last eval_interval iterations
-    for epoch in range(epochs):
+    # Finish the wandb run
+    if cfg["WANDB_LOG"]:
+        wandb.finish()
+
+
+def train(
+    model: nn.Module,
+    loss_func: nn.Module,
+    optimizers: Tuple[Optimizer, Optimizer],
+    schedulers: Tuple[_LRScheduler, _LRScheduler],
+    train_loader: DataLoader,
+    val_loader: DataLoader,
+    data: DrugResponseDataset,
+    cfg: Dict,
+    iter_num: int = 0,
+    best_val_loss: int = 1e9,
+):
+    """Train DruxAI Network end-to-end.
+
+    Args:
+        model (nn.Module): _description_
+        loss_func (nn.Module): _description_
+        optimizers (Tuple): _description_
+        schedulers (Tuple): _description_
+        train_loader (DataLoader): _description_
+        val_loader (DataLoader): _description_
+        epochs (int): _description_
+        device (torch.device): _description_
+        grad_clip (int): _description_
+        decay_lr (int): _description_
+        cfg (Dict): _description_
+        wandb_log (bool, optional): _description_. Defaults to True.
+        iter_num (int, optional): _description_. Defaults to 0.
+        best_val_loss (int, optional): _description_. Defaults to 1e9.
+    """
+    optimizer1, optimizer2 = optimizers[0], optimizers[1]
+    scheduler1, scheduler2 = schedulers[0], schedulers[1]
+    for epoch in range(cfg["EPOCHS"]):
         model.train()
-
         for X, y, _ in tqdm(train_loader):
-
             drug, molecular, outcome = (
-                X["drug_encoding"].to(device),
-                X["gene_expression"].to(device),
-                y.to(device),
+                X["drug_encoding"].to(cfg["DEVICE"]),
+                X["gene_expression"].to(cfg["DEVICE"]),
+                y.to(cfg["DEVICE"]),
             )
 
             optimizer1.zero_grad()
             optimizer2.zero_grad()
 
             prediction = model.forward(drug, molecular)
-            loss = criterion(outcome, prediction)
-            if decay_lr:
-                lr = get_lr(iter_num, warmup_iters, lr_decay_iters, learning_rate, min_lr)
-                for param_group1, param_group2 in zip(optimizer1.param_groups, optimizer2.param_groups):
-                    param_group1["lr"] = lr
-                    param_group2["lr"] = lr
+            loss = loss_func(outcome, prediction)
 
             loss.backward()
-            clip_grad_norm_(model.parameters(), grad_clip)
+            clip_grad_norm_(model.parameters(), cfg["GRAD_CLIP"])
             # Randomly select optimizer
             selected_optimizer = optimizer1 if torch.rand(1) < 0.5 else optimizer2
             selected_optimizer.step()
-            if (iter_num % eval_interval == 0) & (iter_num != 0):
-                avg_val_loss, r2_val = evaluate_model(model, val_loader, data, epoch)
-                if wandb_log:
+            if cfg["DECAY_LR"]:
+                scheduler1.step()
+                scheduler2.step()
+            if (iter_num % cfg["EVAL_INTERVAL"] == 0) & (iter_num != 0):
+                avg_val_loss, r2_val = evaluate_model(model, val_loader, data, epoch, cfg, loss_func)
+                if cfg["WANDB_LOG"]:
                     wandb.log(
                         {
                             "iter": iter_num,
@@ -314,18 +291,64 @@ def train():
                         "iter_num": iter_num,
                         "epoch": epoch,
                         "best_val_loss": best_val_loss,
-                        "config": config,
+                        "config": cfg,
                     }
                     logger.info(
-                        f"New best val_loss achieved. Saving checkpoint to {os.path.join(RESULTS_PATH, 'ckpt.pt')}"
+                        f"New best val_loss achieved. Saving checkpoint to "
+                        f"{os.path.join(cfg['RESULTS_PATH'], 'ckpt.pt')}"
                     )
-                    torch.save(checkpoint, os.path.join(RESULTS_PATH, "ckpt.pt"))
+                    torch.save(checkpoint, os.path.join(cfg["RESULTS_PATH"], "ckpt.pt"))
             iter_num += 1
 
-    # Finish the wandb run
-    if wandb_log:
-        wandb.finish()
+
+def check_cuda_devices(logger):
+    """Print information about available GPUs."""
+    num_devices = torch.cuda.device_count()
+    logger.info("-" * 50)
+    if num_devices > 1:
+        logger.info(f"{num_devices} GPUs are available:")
+    elif num_devices == 1:
+        logger.info("1 GPU is available:")
+    else:
+        logger.info("WARNING! NO GPU DETECTED!")
+        logger.info("-" * 50)
+        return
+
+    for i in range(num_devices):
+        props = torch.cuda.get_device_properties(i)
+        logger.info(f"{i} | {props.name:<20} | {props.total_memory / 1024**3:2.2f} GB Memory")
+    logger.info("-" * 50)
+
+
+def read_yml(filepath: str) -> dict:
+    """Load a yml file to memory as dict."""
+    with open(filepath) as ymlfile:
+        return dict(yaml.load(ymlfile, Loader=yaml.FullLoader))
+
+
+def main() -> None:
+    """Execute main func."""
+    # Get correct config file over command line.
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "-c",
+        "--config",
+        default="/Users/niklaskiermeyer/Desktop/Codespace/DruxAI/results/training/config.yml",
+        type=str,
+    )
+    args = parser.parse_args()
+
+    # Read yml file to memory as dict.
+    cfg = read_yml(args.config)
+
+    logger = _setup_logger(log_path=cfg["RESULTS_PATH"])
+
+    # Get information about allocated GPUs
+    check_cuda_devices(logger)
+
+    # Start training with chosen configuration.
+    run(cfg, logger)
 
 
 if __name__ == "__main__":
-    train()
+    main()
